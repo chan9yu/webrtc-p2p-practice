d4074bb85d2914ffe04cdc20f43745995656a73b
chan9yu <dev.cgyeo@gmail.com>
2025-03-04 13:00:31 +0900

    chore: backend workspace 구축

---
+++ backend/eslint.config.js
@@ -0,0 +1,30 @@
+import js from '@eslint/js';
+import tsParser from '@typescript-eslint/parser';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import prettier from 'eslint-plugin-prettier';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+const config = tseslint.config({
+	extends: [js.configs.recommended, ...tseslint.configs.recommended, eslintConfigPrettier],
+	files: ['**/*.{ts,tsx}'],
+	languageOptions: {
+		parser: tsParser,
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		globals: globals.node
+	},
+	plugins: {
+		prettier,
+		'simple-import-sort': simpleImportSort
+	},
+	rules: {
+		'prettier/prettier': 'error',
+		'simple-import-sort/imports': 'error',
+		'simple-import-sort/exports': 'error'
+	}
+});
+
+export default config;
